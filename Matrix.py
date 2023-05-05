2b975f221d66c986ccbd1b0a38af5b2b5e21ecc9
PatrickDoolittle <85011119+PatrickDoolittle@users.noreply.github.com>
2023-05-05 10:00:15 -0700

    Fixed indexing into a matrix

--- Matrix.py
+++ Matrix.py
@@ -24,10 +24,7 @@ class Matrix:
     def __getitem__(self,indices):
         if isinstance(indices, int):
             return self.vectors[indices]
-        if len(indices) != 2:
-            raise IndexError("Matrix needs 1 or 2 indices")   
-        i1, i2 = indices
-        return self.vectors[i1][i2]
+
 
     def transpose(self):
         new_vectors = []
@@ -37,24 +34,3 @@ class Matrix:
                 row.append(self[(i,j)])
             new_vectors.append(Vector(row))
         return Matrix(new_vectors)
-
-
-'''
-Unit test for Matrix module
-'''
-v1 = Vector([1,2,3])
-v2 = Vector([4,5,6])
-v3 = Vector([7,8,9])
-vectors = [v1,v2,v3]
-print("Matrix initialization test")
-try:
-    A = Matrix(vectors)
-except Exception as e:
-    raise Exception("fail")
-
-A = Matrix(vectors)
-print("pass\n")
-
-print("Index test")
-print(str(A[0][0]))
-
