ef569ac613dd2ed012e9552af676c7be0cd2f8e0
patrickdoolittle <patrick.doolittle@gmail.com>
2023-05-09 21:42:39 -0700

    Made some changes to add_vectors animation

--- Visualizations.py
+++ Visualizations.py
@@ -20,12 +20,16 @@ def add_vectors(static: Vector, operand: Vector):
     class VectorAddition(Scene):
         def construct(self):
             sum = Vector(static.get_end() + operand.get_end())
+            sum_coordinate = sum.coordinate_label()
             self.add(NumberPlane())
-            self.add(static)
-            self.add(operand)
+            static_coordinates, operand_coordinates = static.coordinate_label(), operand.coordinate_label()
+            self.add(static, operand, static_coordinates, operand_coordinates)
+
             # Show the operand slide up the length of the static vector until the tail of the operand is at the head of the static vector
             self.play(ApplyMethod(operand.shift, static.get_end()))
             # Show the sum vector
             self.play(Create(sum))
+            self.remove(static, operand)
+            self.add(sum_coordinate)
             self.wait(5)
     return VectorAddition
